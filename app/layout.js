537c416d8555b84e377a3095950b86339c4a6e1c
Ugo Chinememerem <ugofranklin22@gmail.com>
2024-07-10 16:38:08 +0100

    add:eventmint_layout

--- app/layout.js
+++ app/layout.js
@@ -9,7 +9,14 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Event Mint",
-  description: "By the students, for the students!",
+  description: "Enhancing Events with Interaction",
+  keywords:
+    "Event Mint, Interactive Events, Event Management, Event Technology, Interactive Features, Seamless Interaction",
+  author: "Event Mint Team",
+  viewport: "width=device-width, initial-scale=1.0",
+  url: "https://eventmint.xyz",
+  image:
+    "https://res.cloudinary.com/dtfvdjvyr/image/upload/v1719802304/event-logo_iyl1ec.png",
 };
 
 export default async function RootLayout({ children }) {
@@ -30,6 +37,30 @@ export default async function RootLayout({ children }) {
 
   return (
     <html lang="en">
+      <head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <meta name="keywords" content={metadata.keywords} />
+        <meta name="author" content={metadata.author} />
+        <meta name="viewport" content={metadata.viewport} />
+        <link
+          rel="icon"
+          href="https://res.cloudinary.com/dtfvdjvyr/image/upload/v1720387059/evenmint0_yeqwyy.png"
+        />
+        {/* Open Graph Meta Tags */}
+        <meta property="og:title" content={metadata.title} />
+        <meta property="og:description" content={metadata.description} />
+        <meta property="og:url" content={metadata.url} />
+        <meta property="og:image" content={metadata.image} />
+        <meta property="og:type" content="website" />
+        {/* Twitter Card Meta Tags */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content={metadata.title} />
+        <meta name="twitter:description" content={metadata.description} />
+        <meta name="twitter:image" content={metadata.image} />
+        <meta name="twitter:url" content={metadata.url} />
+        {/* Add more meta tags as needed */}
+      </head>
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
